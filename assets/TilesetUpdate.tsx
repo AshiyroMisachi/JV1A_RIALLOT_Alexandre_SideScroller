c22219b876b3a1599c4d73d1e5d85eba57a88cf5
AshiyroMisachi <riallot.alexandre@outlook.fr>
2023-01-13 13:19:48 +0100

    Ok on voit le NiveauTiled
    Probleme avec les Plateformes

--- assets/TilesetUpdate.tsx
+++ assets/TilesetUpdate.tsx
@@ -1,5 +1,5 @@
 <?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.9" tiledversion="1.9.2" name="TEst" tilewidth="32" tileheight="32" tilecount="2500" columns="50">
+<tileset version="1.9" tiledversion="1.9.2" name="TilesetUpdate" tilewidth="32" tileheight="32" tilecount="2500" columns="50">
  <image source="TilesetFinal.png" width="1600" height="1600"/>
  <tile id="51">
   <properties>
